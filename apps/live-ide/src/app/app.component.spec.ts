c543385ff35674591d01f5e44ed286370a5e127f
Kay - Khanh Bùi <khanhbui.lab@gmail.com>
2020-12-28 18:25:31 +0700

    refactor(repo): fix eslint errors

--- apps/live-ide/src/app/app.component.spec.ts
+++ apps/live-ide/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TestBed } from '@angular/core/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,21 +12,21 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'live-ide'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.componentInstance;
     expect(app.title).toEqual('live-ide');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to live-ide!');
+    const compiled: HTMLElement = fixture.nativeElement;
+    expect(compiled?.querySelector('h1')?.textContent).toContain('Welcome to live-ide!');
   });
 });

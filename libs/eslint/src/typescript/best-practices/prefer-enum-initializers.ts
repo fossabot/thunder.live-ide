9bc1d5659a63d34f72c0b5047eaf39193f0c2b05
Phong Cao <phongcao3091998@gmail.com>
2021-01-08 01:41:02 +0700

    docs(eslint): apply eslint rules: prefer enum initializer

--- libs/eslint/src/typescript/best-practices/prefer-enum-initializers.ts
+++ libs/eslint/src/typescript/best-practices/prefer-enum-initializers.ts
@@ -1,6 +1,40 @@
+// Bypass these rules to focus on the examples
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/naming-convention */
+
 // COMMENT THE DISABLED ESLINT RULES BELOW TO SEE MORE ERRORS
 /* eslint-disable @typescript-eslint/prefer-enum-initializers */
 
 // "@typescript/eslint/prefer-enum-initializers": ["error"],
 // Prefer initializing each enums member value
 // https://github.com/typescript-eslint/typescript-eslint/blob/v4.10.0/packages/eslint-plugin/docs/rules/prefer-enum-initializers.md
+
+
+// Bad
+enum BadExample {
+  FirstValue, // <= Must initializing value
+  SecondValue // <= Must initializing value
+}
+enum BadColor {
+  Green = 'Green',
+  // @ts-expect-error: Expect error occurs
+  Red // <= Must initializing value
+}
+enum BadDirection {
+  Up, // <= Must initializing value
+  Down = 2
+}
+
+// Good
+enum GoodExample {
+  FirstValue = 'FirstValue',
+  SecondValue = 'SecondValue'
+}
+enum GoodColor {
+  Green = 'Green',
+  Red = 'Red'
+}
+enum GoodDirection {
+  Up = 1,
+  Down = 2
+}

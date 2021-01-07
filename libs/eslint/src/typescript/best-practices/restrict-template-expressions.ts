1b05f3dd60465ace28f6ab346064cceffeb1d3ee
Phong Cao <phongcao3091998@gmail.com>
2021-01-08 00:45:35 +0700

    docs(eslint): add code example for restrict template expressions rule

--- libs/eslint/src/typescript/best-practices/restrict-template-expressions.ts
+++ libs/eslint/src/typescript/best-practices/restrict-template-expressions.ts
@@ -4,3 +4,23 @@
 // "@typescript/eslint/restrict-template-expressions": ["error",{"allowNumber":true}],
 // Enforce template literal expressions to be of string type
 // https://github.com/typescript-eslint/typescript-eslint/blob/v4.10.0/packages/eslint-plugin/docs/rules/restrict-template-expressions.md
+
+// BAD
+const arrayVariable: string[] = ['item1', 'item2'];
+console.log(`${arrayVariable} is here`);
+
+// GOOD
+const stringVariable: string = 'another string';
+const numberVariable: number = 1;
+
+console.log(`${stringVariable} is here`);
+console.log(`${stringVariable || 'default value'} is here`);
+console.log(`${numberVariable.toString()} is here`);
+
+// Option:
+// allowNumber: true (default),
+// allowBoolean: false (default),
+// allowAny: false (default),
+// allowNullish: false (default),
+
+

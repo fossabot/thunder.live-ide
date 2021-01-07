d1f24a651c59e156691b04be980489910dca50fe
Phong Cao <phongcao3091998@gmail.com>
2021-01-07 21:59:39 +0700

    docs(eslint): add eslint rule: prefer regexp exec
    BREAKING CHANGE:
    Apply prefer enum initializing
    BREAKING CHANGE:
    Apply Eslint Rule:
    Prefer
    Enum
    Initializers

--- libs/eslint/src/typescript/best-practices/prefer-regexp-exec.ts
+++ libs/eslint/src/typescript/best-practices/prefer-regexp-exec.ts
@@ -4,3 +4,16 @@
 // "@typescript/eslint/prefer-regexp-exec": ["error"],
 // Enforce that `RegExp#exec` is used instead of `String#match` if no global flag is provided
 // https://github.com/typescript-eslint/typescript-eslint/blob/v4.10.0/packages/eslint-plugin/docs/rules/prefer-regexp-exec.md
+
+
+// Bad
+'some text'.match(/text/);
+'some text are just texts'.match(/text/);
+
+// Good
+/text/.exec('some text');
+'some text are just texts'.match(/text/g);
+
+// When using String.match() without flag 'g'
+// It return the same output with RegExp.exec() but it takes a large amount performance
+

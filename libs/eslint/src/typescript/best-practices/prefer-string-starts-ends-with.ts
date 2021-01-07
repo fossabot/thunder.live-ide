ccfc7fc495d93e4498d9ebf9783565086ba32df6
Phong Cao <phongcao3091998@gmail.com>
2021-01-08 06:03:15 +0700

    docs(eslint): update eslint rule prefer string start end with

--- libs/eslint/src/typescript/best-practices/prefer-string-starts-ends-with.ts
+++ libs/eslint/src/typescript/best-practices/prefer-string-starts-ends-with.ts
@@ -1,8 +1,3 @@
-// Bypass these rules to focus on the example
-/* eslint-disable @typescript-eslint/prefer-regexp-exec */
-/* eslint-disable @thunder-angular/no-magic-numbers */
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-
 // COMMENT THE DISABLED ESLINT RULES BELOW TO SEE MORE ERRORS
 /* eslint-disable @typescript-eslint/prefer-string-starts-ends-with */
 
@@ -10,28 +5,32 @@
 // Enforce the use of `String#startsWith` and `String#endsWith` instead of other equivalent methods of checking substrings
 // https://github.com/typescript-eslint/typescript-eslint/blob/v4.10.0/packages/eslint-plugin/docs/rules/prefer-string-starts-ends-with.md
 
-
 const stringVariable: string = '';
 
 // BAD
-
 // Check string starts with
-stringVariable[0] === 'b';
-stringVariable.charAt(0) === 'b';
-stringVariable.indexOf('text') === 0;
-stringVariable.slice(0, 3) === 'text';
-stringVariable.substring(0, 3) === 'text';
-stringVariable.match(/^text/) != null;
-/^text/.test(stringVariable);
+let isStartWith: boolean = false;
+const firstIndex: number = 0;
+const thirdIndex: number = 3;
+isStartWith = stringVariable[firstIndex] === 'b';
+isStartWith = stringVariable.charAt(firstIndex) === 'b';
+isStartWith = stringVariable.indexOf('text') === firstIndex;
+isStartWith = stringVariable.slice(firstIndex, thirdIndex) === 'text';
+isStartWith = stringVariable.substring(firstIndex, thirdIndex) === 'text';
+isStartWith = /^text/.test(stringVariable);
+console.log(isStartWith);
 
 // Check string ends with
-stringVariable[stringVariable.length - 1] === 'b';
-stringVariable.charAt(stringVariable.length - 1) === 'b';
-stringVariable.lastIndexOf('text') === stringVariable.length - 3;
-stringVariable.slice(-3) === 'text';
-stringVariable.substring(stringVariable.length - 3) === 'text';
-stringVariable.match(/text$/) != null;
-/text$/.test(stringVariable);
+let isEndWith: boolean = false;
+const oneItemUnit: number = 1;
+const threeItemUnit: number = 3;
+isEndWith = stringVariable[stringVariable.length - oneItemUnit] === 'b';
+isEndWith = stringVariable.charAt(stringVariable.length - oneItemUnit) === 'b';
+isEndWith = stringVariable.lastIndexOf('text') === stringVariable.length - threeItemUnit;
+isEndWith = stringVariable.slice(-threeItemUnit) === 'text';
+isEndWith = stringVariable.substring(stringVariable.length - threeItemUnit) === 'text';
+isEndWith = /text$/.test(stringVariable);
+console.log(isEndWith);
 
 // GOOD
 stringVariable.startsWith('text');

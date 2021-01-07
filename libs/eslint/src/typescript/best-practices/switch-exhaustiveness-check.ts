8ba48a030e62cf0eb2cb6024d31d651a5dc7c2ef
Phong Cao <phongcao3091998@gmail.com>
2021-01-08 05:49:22 +0700

    docs(eslint): apply eslint rule switch exhaustiveness check

--- libs/eslint/src/typescript/best-practices/switch-exhaustiveness-check.ts
+++ libs/eslint/src/typescript/best-practices/switch-exhaustiveness-check.ts
@@ -4,3 +4,55 @@
 // "@typescript/eslint/switch-exhaustiveness-check": ["error"],
 // Exhaustiveness checking in switch with union type
 // https://github.com/typescript-eslint/typescript-eslint/blob/v4.10.0/packages/eslint-plugin/docs/rules/switch-exhaustiveness-check.md
+
+enum WorkingDay {
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday'
+}
+
+const getWorkingDay = (): WorkingDay | undefined => undefined;
+const workingDay: WorkingDay | undefined = getWorkingDay();
+
+// BAD
+switch (workingDay) {
+  case WorkingDay.Monday:
+    // Do something
+    break;
+  case WorkingDay.Tuesday:
+    // Do something
+    break;  // <= Bad, not cover all cases of WorkingDay
+}
+
+// GOOD
+switch (workingDay) {
+  case WorkingDay.Monday:
+    // Do something
+    break;
+  case WorkingDay.Tuesday:
+    // Do something
+    break;
+  case WorkingDay.Wednesday:
+    // Do something
+    break;
+  case WorkingDay.Thursday:
+    // Do something
+    break;
+  case WorkingDay.Friday:
+    // Do something
+    break;
+  case undefined:
+    // Do something
+    break;
+}
+
+switch (workingDay) {
+  case WorkingDay.Monday:
+    // Do something
+    break;
+  default: // <= add a default case
+    // Do something
+    break;
+}
